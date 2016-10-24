248b6a1516544da609e972f9b37923bc25d96866
Dmitri Aizenberg <dmitri.aiz@gmail.com>
2016-10-24 15:08:53 -0700

    added Bootstrap

--- App.js
+++ App.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Grid, Row, Col } from 'react-bootstrap';
 
-class App extends React.Component{
+/*class App extends React.Component{
   render() {
-    return <div>Hi</div>;
+    return <Button>Hi</Button>;
   }
-}
+}*/
+
+const App = () => (
+  <Grid>
+    <Row className="show-grid">
+      <Col xs={12} md={8}>Search</Col>
+      <Col xs={6} md={4}>Sign In</Col>
+    </Row>
+
+    <Row className="show-grid">
+    </Row>
+  </Grid>
+);
 
 export default App;
